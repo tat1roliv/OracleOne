39b8c726adae879cdad512f65ef2fbdbbaa63877
tati oliveira <tat1roliv@gmail.com>
2021-01-30 12:35:44 -0300

    javaScript_2_4

--- oo_JavaScript/welfare.js
+++ oo_JavaScript/welfare.js
@@ -1,8 +1,29 @@
-console.log('hola');
 
-let titulo = document.querySelector(".titulo");
+//let titulo = document.querySelector(".titulo");
+//titulo.textContent = "health care";
 
-console.log(titulo);
-console.log(titulo.textContent);
+//busca geral nos dados do primeiro paciente
+let paciente = document.querySelector("#primeiroPaciente");
 
-titulo.textContent = "health care";
+//dados peso
+//busca no conteudo da id a classe peso
+let tdPeso = paciente.querySelector(".info-peso");
+//busca o valor do peso na td (textContent), como string?
+let pesoPaciente = (tdPeso.textContent);
+
+
+//console = peso paciente 1
+console.log(pesoPaciente);
+
+//dados altura
+let tdAltura = paciente.querySelector(".info-altura");
+let alturaPaciente = tdAltura.textContent;
+
+//BMI
+let bmiPaciente = pesoPaciente / (alturaPaciente*alturaPaciente);
+console.log(bmiPaciente);
+
+//exibe bmi
+var tdBmi = paciente.querySelector(".info-imc");
+tdBmi.textContent = bmiPaciente;;
+
