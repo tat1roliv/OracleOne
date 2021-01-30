3209762f77f166b1371339dd66e8623c0fa68bda
tati oliveira <tat1roliv@gmail.com>
2021-01-30 13:10:30 -0300

    javaScript_2_7

--- oo_JavaScript/welfare.js
+++ oo_JavaScript/welfare.js
@@ -11,19 +11,43 @@ let tdPeso = paciente.querySelector(".info-peso");
 //busca o valor do peso na td (textContent), como string?
 let pesoPaciente = (tdPeso.textContent);
 
-
-//console = peso paciente 1
-console.log(pesoPaciente);
-
 //dados altura
 let tdAltura = paciente.querySelector(".info-altura");
 let alturaPaciente = tdAltura.textContent;
 
-//BMI
-let bmiPaciente = pesoPaciente / (alturaPaciente*alturaPaciente);
-console.log(bmiPaciente);
 
-//exibe bmi
-var tdBmi = paciente.querySelector(".info-imc");
-tdBmi.textContent = bmiPaciente;;
+//validacoes
+
+let tdBmi = paciente.querySelector(".info-imc");
+
+let pesoValido = true;
+let alturaValido = true;
+
+//peso
+if (pesoPaciente <= 0 || pesoPaciente > 500){
+    //console.log("Peso inválido");
+    pesoValido = false;
+    tdBmi.textContent = "width error";
+}
+
+//altura
+
+if(alturaPaciente <= 0 || alturaPaciente > 2.5){
+    //console.log("Altura inválida");
+    alturaValido = false;
+    tdBmi.textContent = "height error";
+}
+
+
+//BMI
+if( pesoValido && alturaValido){
+    let bmiPaciente = pesoPaciente / (alturaPaciente*alturaPaciente);
+    //console.log(bmiPaciente);
+
+    //exibe bmi
+    
+    tdBmi.textContent = bmiPaciente;
+}   
+
+
 
