f281b1cfb16aa202455d739435a8fb5c4158ee5d
tati oliveira <tat1roliv@gmail.com>
2021-02-03 16:49:43 -0300

    javaScript_4_6

--- oo_JavaScript/welfare.js
+++ oo_JavaScript/welfare.js
@@ -72,5 +72,47 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente")
 //funcao anonima
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();//previne comportamentos padroes do browser
-    console.log("clickclickclick");
+    //console.log("clickclickclick");
+
+    //incluir novo user from form
+    var  form = document.querySelector("#form-adiciona");
+    //console.log(form.name.value);
+
+    //coletando as informações digitadas
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    /*
+    console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
+    */
+   //criando a variavel para nova tr / linha(new user)
+    var pacienteTr = document.createElement("tr");
+    
+    //criando colunas da nova linha
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var bmiTd = document.createElement("td");
+
+    //inserindo os valores digitados na nova linha (atribuindo o valor correto em cada celula)
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    //colunas-> child da linha
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    //nova linha (new user) -> child da tabela (tbody)
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
 });
