3666ef36424bfdcb210bdecd043bf62b9e84b3f4
tati oliveira <tat1roliv@gmail.com>
2021-02-16 20:05:41 -0300

    javaScript_7_1

--- oo_JavaScript/projeto/assets/js/remove.js
+++ oo_JavaScript/projeto/assets/js/remove.js
@@ -1,2 +1,9 @@
 var pacientes = document.querySelectorAll(".paciente");
 console.log(pacientes);
+
+pacientes.forEach(function(paciente){
+    paciente.addEventListener('dblclick', function(){
+        console.log('duplo clique');
+        this.remove();
+    })
+})
