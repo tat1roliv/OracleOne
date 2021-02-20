41f73d61bee7539655a0e5015ebfd001e23543b8
tati oliveira <tat1roliv@gmail.com>
2021-02-20 15:18:55 -0300

    js8_1

---
+++ oo_JavaScript/projeto/assets/js/filter.js
@@ -0,0 +1,12 @@
+//filter->text
+var campoFiltro = document.querySelector("#filtrar-tabela");
+campoFiltro.addEventListener("input", function(){
+    //console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+    for (var i = 0; i< pacientes.length; i++){
+        var paciente = pacientes[i];
+        var tdNome = paciente.querySelector(".info-nome");
+        var nome = paciente.textContent;
+
+    }
+})
