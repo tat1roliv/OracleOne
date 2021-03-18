2da7ab6a6c5f2a34c2814104969cbb15d149bcc6
tati oliveira <tat1roliv@gmail.com>
2021-03-18 12:05:57 -0300

    java-oo-2

--- oo_Java/bytebank/src/bytebank/Conta.java
+++ oo_Java/bytebank/src/bytebank/Conta.java
@@ -1,7 +1,7 @@
 package bytebank;
 
 public class Conta {
-	
+	//atributos
 	double saldo;
 	int agencia;
 	int numero;
---
+++ oo_Java/bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,12 @@
+package bytebank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.print(primeiraConta.saldo);
+
+	}
+
+}
