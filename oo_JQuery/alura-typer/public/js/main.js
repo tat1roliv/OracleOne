6f9862676475c00ac5f69e309af7dd1355a38317
tati oliveira <tat1roliv@gmail.com>
2021-03-22 18:14:14 -0300

    jquery1-2-1

--- oo_JQuery/alura-typer/public/js/main.js
+++ oo_JQuery/alura-typer/public/js/main.js
@@ -1,2 +1,13 @@
 //console.log("arquivo main");
 
+var frase = $(".frase").text();//$ = jQuery
+var numeroPalavras = frase.split(" ").length;//parte a string atribuindo a uma pos do array criado cada palavra;
+
+//console.log(frase);
+//console.log(numeroPalavras);
+
+var tamanhoFrase = $("#tamanho-frase-tela");
+//console.log(tamanhoFrase);
+tamanhoFrase.text(numeroPalavras);//atribuindo valor capturado
+
+
