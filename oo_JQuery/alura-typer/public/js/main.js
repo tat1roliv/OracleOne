80031afa1d0ad7acdd1e81a999813d6c1ce5f80f
tati oliveira <tat1roliv@gmail.com>
2021-03-22 18:57:09 -0300

    jquery1-2-2

--- oo_JQuery/alura-typer/public/js/main.js
+++ oo_JQuery/alura-typer/public/js/main.js
@@ -1,13 +1,25 @@
 //console.log("arquivo main");
 
 var frase = $(".frase").text();//$ = jQuery
-var numeroPalavras = frase.split(" ").length;//parte a string atribuindo a uma pos do array criado cada palavra;
+var numPalavras = frase.split(" ").length;//parte a string atribuindo a uma pos do array criado cada palavra;
 
 //console.log(frase);
 //console.log(numeroPalavras);
 
-var tamanhoFrase = $("#tamanho-frase-tela");
+var tamanhoFrase = $("#tamanho-frase");
 //console.log(tamanhoFrase);
-tamanhoFrase.text(numeroPalavras);//atribuindo valor capturado
+tamanhoFrase.text(numPalavras);//atribuindo valor capturado
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+   var conteudo = campo.val();
+
+   var qtdPalavras = conteudo.split(/\S+/).length -1;//expressao regular para espaços vazios na string => (/\S+/)
+   $("#contador-palavras").text(qtdPalavras);
+
+   var qtdCaracteres = conteudo.length;
+   $("#contador-caracteres").text(qtdCaracteres);
+
+});
 
 
