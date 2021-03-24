d20e3884944741f4538d14a77ef3cd337cc3df8e
tati oliveira <tat1roliv@gmail.com>
2021-03-24 14:28:47 -0300

    update

---
+++ #ImersaoDev/Calculator/main.js
@@ -0,0 +1,18 @@
+function addNumber(number) {
+    
+    if (number != '/' && number != '*' && number != '+' && number != '-') {
+        alert("Inserir primeiro o valor numérico.");
+    }
+    
+    else{
+
+
+    }
+}
+
+
+  // Declaração da função que executa a operação já escrita
+  function addEqual() {
+    var content = document.querySelector('.result').value;
+
+  }
