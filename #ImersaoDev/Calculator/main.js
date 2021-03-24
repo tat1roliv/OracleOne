12cdb4a69e33d971d6b432668cc7eea1c1a07666
tati oliveira <tat1roliv@gmail.com>
2021-03-24 14:29:27 -0300

    Update main.js

--- #ImersaoDev/Calculator/main.js
+++ #ImersaoDev/Calculator/main.js
@@ -16,3 +16,34 @@ function addNumber(number) {
     var content = document.querySelector('.result').value;
 
   }
+  //operações matematicas basicas
+  
+  function soma(numeroA,numeroB){
+      return numeroA+numeroB
+    }
+    
+    function sub(numeroA,numeroB){
+      return numeroA-numeroB
+    }
+    
+    function mult(numeroA,numeroB){
+      return numeroA*numeroB
+    }
+    
+    function div(dividendo,divisor){
+      if(divisor == 0){
+        return 'Erro de divisão por zero'
+      }
+      else{
+        return dividendo/divisor
+      }
+    }
+    
+    //bloco de instrução
+    var numeroA = parseInt(prompt('Numero A: '))
+    var numeroB = parseInt(prompt('Numero B: '))
+    
+    alert('Soma: ' + soma(numeroA,numeroB))
+    alert('Subtração: ' + sub(numeroA,numeroB))
+    alert('Multiplicação: ' + mult(numeroA,numeroB))
+    alert('Divisão: ' + div(numeroA,numeroB))
