de02f074bda36df3f11a480c30ba1d85f9467c00
tati oliveira <tat1roliv@gmail.com>
2021-03-29 10:05:59 -0300

    Update main.js

--- #ImersaoDev/Sale/main.js
+++ #ImersaoDev/Sale/main.js
@@ -19,3 +19,80 @@ var buttonGenerate = document.querySelector('.boxNewText');
 buttonGenerate.addEventListener("click", function(){
     changeData();
 });
+
+/*
+
+ function sorteia(){
+            return Math.round( Math.random()*10 );
+        }
+
+        function sorteiaNumeros(quantidade){
+
+            var segredos = [];
+
+            var n = 1;
+            
+            while ( n <= quantidade ){
+
+                var nAleatorio = sorteia();
+
+                var achou = false;
+                
+                if (nAleatorio !== 0 ){
+
+                    for (var posicao = 0; posicao < segredos.length ; posicao++){
+                        if (segredos [posicao] == nAleatorio){
+                            achou = true;
+                            break;
+                        }
+
+                    }
+                    if (achou == false){
+                        segredos.push(nAleatorio);
+                        n++;
+                    }
+                }
+
+            }
+            return segredos;
+            
+        }
+        
+       
+
+        var segredos = sorteiaNumeros(3);//numero de posições do array
+        console.log(segredos);//exibe numeros sorteados para o array 
+
+        var input = document.querySelector("input");
+
+        input.focus();
+
+        function verifica() {
+            
+            var achou  = false;
+
+            for (posicao = 0; posicao <= segredos.length; posicao++) {
+                
+                if(input.value == segredos[posicao]) {
+
+                    alert("Gol!");
+                    achou = true;
+                    break;
+
+                }
+
+                if (achou == false){
+                    alert("Fora!");
+                }
+            input.value = "";
+            input.focus();     
+            }
+
+        }
+   
+         var button = document.querySelector("button");
+
+         button.onclick = verifica;
+
+
+*/
