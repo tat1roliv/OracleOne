29efed1ce15ade864e23dc6c041058d2614f513c
tati oliveira <tat1roliv@gmail.com>
2021-03-30 12:31:32 -0300

    jquery2-2-2

--- oo_JQuery_2/alura-typer/public/js/frase.js
+++ oo_JQuery_2/alura-typer/public/js/frase.js
@@ -1,5 +1,11 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria); //GET POST...
+}
+
+function trocaFraseAleatoria(data){
+    var frase = $(".frase");
+    var nAleatorio = Math.floor(Math.random()*data.length);//o valor maxim = tamanho do array recebido
+    frase.text(data[nAleatorio].texto);
 }
