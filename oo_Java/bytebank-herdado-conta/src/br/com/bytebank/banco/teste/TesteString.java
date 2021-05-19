3b1b2391b82e5415922fa1c3b292da35d9dac59e
tati oliveira <tat1roliv@gmail.com>
2021-05-19 17:27:38 -0300

    Update TesteString.java

--- oo_Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ oo_Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -1,11 +1,39 @@
 package br.com.bytebank.banco.teste;
 
+
+/**
+ * methods:
+ * 	replace;
+ * 	upperCase;
+ * 	lowerCase;
+ * 	indexOf;
+ * 	charAt;
+ * 	length;
+ * 	isEmpty;
+ * 	contains;
+ * 
+ *  * @author tatio
+ *
+ */
+
+
 public class TesteString {
 
 	public static void main(String[] args) {
 		
+		
+		String vazio = " ";
+		String outroVazio = vazio.trim();
+		
+		
+		System.out.println(vazio.isEmpty());
+		System.out.println(outroVazio.isEmpty());
+		
+	
 		String nome = "Ada";//objeto literal
 		//String outroNome = new String("ada2");
+
+		System.out.println(nome.contains("da"));
 		
 		char c = 'o';
 		
