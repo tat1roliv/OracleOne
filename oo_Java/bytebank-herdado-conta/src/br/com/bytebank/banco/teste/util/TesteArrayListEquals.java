dd029266e1588d104e06f9924fa5d9c382cfa250
tati oliveira <tat1roliv@gmail.com>
2021-05-25 20:13:03 -0300

    LinkedList

--- oo_Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayListEquals.java
+++ oo_Java/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayListEquals.java
@@ -1,6 +1,8 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -9,8 +11,10 @@ public class TesteArrayListEquals {
 
 	public static void main(String[] args) {
 		
+		//Arraylist x LinkedList X list
+		
 		//Generics		
-		ArrayList<Conta> lista = new ArrayList<Conta>();
+		List<Conta> lista = new LinkedList<Conta>();
 					
 		Conta cc = new ContaCorrente(22,11);
 		lista.add(cc);
