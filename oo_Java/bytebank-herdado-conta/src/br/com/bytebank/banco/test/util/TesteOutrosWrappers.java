10e38484ae6088f51d238196a2060f419f91c2f1
tati oliveira <tat1roliv@gmail.com>
2021-05-31 17:50:26 -0300

    lambda func

--- oo_Java/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteOutrosWrappers.java
+++ oo_Java/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteOutrosWrappers.java
@@ -7,10 +7,8 @@ public class TesteOutrosWrappers {
 
 	public static void main(String[] args) {
 		
-		
 		Integer idadeRef = Integer.valueOf(29); //autoboxing
 		System.out.println(idadeRef.intValue()); // unboxing
-		
 		
 		Double dRef = Double.valueOf(3.2); //autoboxing
 		System.out.println(dRef.doubleValue()); //unboxing
