c5239db64b084bd0f35d3cc568fe9a39bc667dbb
tati oliveira <tat1roliv@gmail.com>
2021-06-01 18:59:24 -0300

    entrada dados

--- oo_Java/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
+++ oo_Java/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -4,21 +4,29 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 
 	public static void main(String[] args) throws IOException {
 		
+		//padrao decorator -> um objeto decora o anterior
 		//fluxo de entrada com Arquivo	
-		FileInputStream fis = new FileInputStream("lorem.txt");//file //string
-		InputStreamReader isr = new InputStreamReader(fis);
-		BufferedReader br = new BufferedReader(isr);
+		InputStream fis = new FileInputStream("lorem.txt");//file //string
+		Reader isr = new InputStreamReader(fis);//bytes em caracteres
+		BufferedReader br = new BufferedReader(isr);//juntar caracteres
 		
+		//ex pdf -> stream x reader
 		
 		String linha = br.readLine();
 		
-		System.out.println(linha);
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		
 		
 		br.close();
 		
