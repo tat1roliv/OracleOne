037bdb1f30f7a6f5936f9e5326a8dd8a7a9ed26a
tati oliveira <tat1roliv@gmail.com>
2021-06-02 18:57:21 -0300

    writer

--- oo_Java/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
+++ oo_Java/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -1,34 +1,39 @@
 package br.com.alura.java.io.teste;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 
 public class TesteLeitura {
 
-	public static void main(String[] args) throws IOException {
-		
-		//padrao decorator -> um objeto decora o anterior
-		//fluxo de entrada com Arquivo	
-		InputStream fis = new FileInputStream("lorem.txt");//file //string
-		Reader isr = new InputStreamReader(fis);//bytes em caracteres
-		BufferedReader br = new BufferedReader(isr);//juntar caracteres
-		
-		//ex pdf -> stream x reader
-		
-		String linha = br.readLine();
-		
-		while (linha != null) {
-			System.out.println(linha);
-			linha = br.readLine();
-		}
-		
-		
-		br.close();
+		public static void main(String[] args) throws IOException {
+			
+			//padrao decorator -> um objeto decora o anterior
+			//fluxo de entrada com Arquivo	
+			InputStream fis = new FileInputStream("lorem.txt");//file //string
+			Reader isr = new InputStreamReader(fis);//bytes em caracteres
+			BufferedReader br = new BufferedReader(isr);//juntar caracteres
+			
+			//ex pdf -> stream x reader (txt)
+			
+			String linha = br.readLine();
+			
+			while (linha != null) {
+				System.out.println(linha);
+				linha = br.readLine();
+			}
+			
+			
+			br.close();
 		
 	}
 
